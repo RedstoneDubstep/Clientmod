9355db16fe9e975a5fd1b8ed7faa894839db5cb5
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2020-12-05 19:51:53 +0100

    Fixed the "incompatible mod list" message in the server list

--- src/main/java/net/redstonedubstep/clientmod/Clientmod.java
+++ src/main/java/net/redstonedubstep/clientmod/Clientmod.java
@@ -1,9 +1,13 @@
 package net.redstonedubstep.clientmod;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod(Clientmod.MODID)
 @EventBusSubscriber(modid=Clientmod.MODID, bus= Bus.MOD)
@@ -12,6 +16,7 @@ public class Clientmod {
 	public static final String MODID = "clientmod";
 
 	public Clientmod() {
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, ()->Pair.of(()->"Placeholder string", (remoteversionstring,networkbool)->networkbool));
 		MinecraftForge.EVENT_BUS.register(new ClientEventHandler());
 	}
 }

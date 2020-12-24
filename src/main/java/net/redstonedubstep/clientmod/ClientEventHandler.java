017eec34591aff7954145cfd2c24d94ea01a5a30
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2020-12-24 10:48:32 +0100

    Moved listeners around and added a detection for the "Minceraft" screen

--- src/main/java/net/redstonedubstep/clientmod/ClientEventHandler.java
+++ src/main/java/net/redstonedubstep/clientmod/ClientEventHandler.java
@@ -1,28 +1,38 @@
 package net.redstonedubstep.clientmod;
 
+import java.lang.reflect.Field;
+
+import cpw.mods.modlauncher.api.INameMappingService.Domain;
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.api.distmarker.Dist;
+import net.minecraft.client.audio.SimpleSound;
+import net.minecraft.client.gui.screen.MainMenuScreen;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.client.event.GuiScreenEvent.InitGuiEvent;
 import net.minecraftforge.event.TickEvent.ClientTickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.redstonedubstep.clientmod.command.CommandLibrary;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.redstonedubstep.clientmod.misc.KeyBindings;
 import net.redstonedubstep.clientmod.screen.MainScreen;
 
-@EventBusSubscriber(modid=Clientmod.MODID, bus=Bus.MOD, value=Dist.CLIENT)
+@EventBusSubscriber(modid=Clientmod.MODID)
 public class ClientEventHandler {
-	@SubscribeEvent
-	public static void onFMLClientSetup(FMLClientSetupEvent event) {
-		KeyBindings.init();
-		CommandLibrary.addCommandsToList();
-	}
-
 	@SubscribeEvent
 	public void onClientTick(ClientTickEvent event) {
 		if (KeyBindings.openTextbox.isPressed()) {
 			Minecraft.getInstance().displayGuiScreen(new MainScreen());
+		}
+	}
+
+	//play a sound if the "Minceraft" logo is shown, credits to bl4ckscor3 for that code
+	@SubscribeEvent
+	public static void onInitGuiPost(InitGuiEvent.Post event) {
+		if(event.getGui() instanceof MainMenuScreen) {
+			boolean isTitleWronglySpelled = ObfuscationReflectionHelper.getPrivateValue(MainMenuScreen.class, (MainMenuScreen)event.getGui(), "field_213101_e");
+
+			if (isTitleWronglySpelled) {
+				Minecraft.getInstance().getSoundHandler().play(SimpleSound.master(SoundEvents.ENTITY_WITHER_DEATH,1, 5));
+			}
 		}
 	}
 }
--- src/main/java/net/redstonedubstep/clientmod/Clientmod.java
+++ src/main/java/net/redstonedubstep/clientmod/Clientmod.java
@@ -1,22 +1,30 @@
 package net.redstonedubstep.clientmod;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.redstonedubstep.clientmod.command.CommandLibrary;
+import net.redstonedubstep.clientmod.misc.KeyBindings;
 import org.apache.commons.lang3.tuple.Pair;
 
 @Mod(Clientmod.MODID)
-@EventBusSubscriber(modid=Clientmod.MODID, bus= Bus.MOD)
+@EventBusSubscriber(modid=Clientmod.MODID, bus=Bus.MOD)
 public class Clientmod {
-
 	public static final String MODID = "clientmod";
 
 	public Clientmod() {
 		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, ()->Pair.of(()->"Placeholder string", (remoteversionstring,networkbool)->networkbool));
 		MinecraftForge.EVENT_BUS.register(new ClientEventHandler());
+	}
+
+	@SubscribeEvent
+	public static void onFMLClientSetup(FMLClientSetupEvent event) {
+		KeyBindings.init();
+		CommandLibrary.addCommandsToList();
 	}
 }

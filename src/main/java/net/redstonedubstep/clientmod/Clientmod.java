1098395b6dac540e915cbe58aef2cd3d2d4a3ce6
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2021-09-22 20:09:40 +0200

    Fixed ExtensionPoint

--- src/main/java/net/redstonedubstep/clientmod/Clientmod.java
+++ src/main/java/net/redstonedubstep/clientmod/Clientmod.java
@@ -12,7 +12,6 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
 import net.redstonedubstep.clientmod.command.CommandLibrary;
 import net.redstonedubstep.clientmod.misc.KeyBindings;
 
@@ -22,7 +21,7 @@ public class Clientmod {
 	public static final String MODID = "clientmod";
 
 	public Clientmod() {
-		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> "", (a, b) -> b));
 		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ClientSettings.CLIENT_SPEC);
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(ClientEventHandler::onFMLLoadComplete);
 		MinecraftForge.EVENT_BUS.register(new ClientEventHandler());

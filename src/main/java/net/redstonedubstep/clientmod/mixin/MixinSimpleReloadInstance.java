91736f0028e6a30e7f3a816617485faa1bad47f3
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2022-01-03 21:25:00 +0100

    Added way to make reloading progress tracker more verbose

---
+++ src/main/java/net/redstonedubstep/clientmod/mixin/MixinSimpleReloadInstance.java
@@ -0,0 +1,26 @@
+package net.redstonedubstep.clientmod.mixin;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.server.packs.resources.PreparableReloadListener;
+import net.minecraft.server.packs.resources.PreparableReloadListener.PreparationBarrier;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.SimpleReloadInstance;
+import net.minecraft.util.profiling.ProfilerFiller;
+import net.redstonedubstep.clientmod.ClientSettings;
+
+@Mixin(SimpleReloadInstance.class)
+public class MixinSimpleReloadInstance {
+	@Redirect(method = "lambda$of$0", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/resources/PreparableReloadListener;reload(Lnet/minecraft/server/packs/resources/PreparableReloadListener$PreparationBarrier;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;Lnet/minecraft/util/profiling/ProfilerFiller;Ljava/util/concurrent/Executor;Ljava/util/concurrent/Executor;)Ljava/util/concurrent/CompletableFuture;"))
+	private static CompletableFuture<Void> redirectReload(PreparableReloadListener instance, PreparationBarrier preparationBarrier, ResourceManager resourceManager, ProfilerFiller profilerFiller, ProfilerFiller secondProfileFiller, Executor backgroundExecutor, Executor gameExecutor, Executor unusedBackgroundExecutor, PreparationBarrier unusedBarrier, ResourceManager unusedResourceManager, PreparableReloadListener unusedReloadListener, Executor correctBackgroundExecutor) {
+		if (ClientSettings.CONFIG.enhancedReloadingInfo.get())
+			backgroundExecutor = correctBackgroundExecutor;
+		
+		return instance.reload(preparationBarrier, resourceManager, profilerFiller, secondProfileFiller, backgroundExecutor, gameExecutor);
+	}
+}

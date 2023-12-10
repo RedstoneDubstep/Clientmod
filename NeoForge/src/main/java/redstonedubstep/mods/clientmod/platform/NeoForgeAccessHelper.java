cb3e37ec3cda50099d3d01ca1cb58a6c96780434
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2023-12-10 17:31:48 +0100

    Updated to 1.20.4

--- NeoForge/src/main/java/redstonedubstep/mods/clientmod/platform/NeoForgeAccessHelper.java
+++ NeoForge/src/main/java/redstonedubstep/mods/clientmod/platform/NeoForgeAccessHelper.java
@@ -4,6 +4,7 @@ import java.nio.file.Path;
 import java.util.Map;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.chunk.SectionRenderDispatcher;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -19,7 +20,9 @@ public class NeoForgeAccessHelper extends AccessHelper {
     public void populateBECountMap(Map<BlockEntityType<?>, Integer> map, Minecraft mc) {
         for (SectionRenderDispatcher.RenderSection section : mc.levelRenderer.visibleSections) {
             for (BlockEntity be : section.getCompiled().getRenderableBlockEntities()) {
-                if (mc.levelRenderer.cullingFrustum.isVisible(be.getRenderBoundingBox())) {
+                BlockEntityRenderer<BlockEntity> renderer = mc.getBlockEntityRenderDispatcher().getRenderer(be);
+
+                if (renderer != null && mc.levelRenderer.cullingFrustum.isVisible(renderer.getRenderBoundingBox(be))) {
                     map.computeIfPresent(be.getType(), (t, i) -> i + 1);
                     map.putIfAbsent(be.getType(), 1);
                 }
@@ -28,7 +31,9 @@ public class NeoForgeAccessHelper extends AccessHelper {
 
         synchronized (mc.levelRenderer.globalBlockEntities) {
             for (BlockEntity be : mc.levelRenderer.globalBlockEntities) {
-                if (mc.levelRenderer.cullingFrustum.isVisible(be.getRenderBoundingBox())) {
+                BlockEntityRenderer<BlockEntity> renderer = mc.getBlockEntityRenderDispatcher().getRenderer(be);
+
+                if (renderer != null && mc.levelRenderer.cullingFrustum.isVisible(renderer.getRenderBoundingBox(be))) {
                     map.computeIfPresent(be.getType(), (t, i) -> i + 1);
                     map.putIfAbsent(be.getType(), 1);
                 }
